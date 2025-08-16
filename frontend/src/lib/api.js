c0ad5260489e6b75e69f65887f3ddd007b6df1f7
Brett Watson <brett.watson@mac.com>
2025-08-16 20:13:28 +1000

    2nd white screen but fixed the sync

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -14,3 +14,30 @@ export const remove = (id) => API.delete(`/veo/jobs/${id}`).then(r => r.data);
 // ---------------------------------------------------------------------------
 export const getQueueStatus = () =>
   API.get('/veo/queue/status').then(r => r.data);
+
+// ---------------------------------------------------------------------------
+// Sync missed videos (download any files that exist remotely but not locally)
+// ---------------------------------------------------------------------------
+export const sync = () =>
+  API.post('/veo/sync').then(r => r.data);
+
+// ---------------------------------------------------------------------------
+// Manual control: Queue processor
+// ---------------------------------------------------------------------------
+export const startQueue = () =>
+  API.post('/veo/queue/start').then(r => r.data);
+
+export const stopQueue = () =>
+  API.post('/veo/queue/stop').then(r => r.data);
+
+// ---------------------------------------------------------------------------
+// Manual control: Background poller
+// ---------------------------------------------------------------------------
+export const startPoller = () =>
+  API.post('/veo/poller/start').then(r => r.data);
+
+export const stopPoller = () =>
+  API.post('/veo/poller/stop').then(r => r.data);
+
+export const getPollerStatus = () =>
+  API.get('/veo/poller/status').then(r => r.data);
